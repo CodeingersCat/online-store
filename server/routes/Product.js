2e10e46ba2f16699ea01f1ec04c9be303c77351a
Shrawan Baruah <rocketpilot763@gmail.com>
2022-10-15 02:31:05 +0530

    clean slate

--- server/routes/Product.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { allProduct, getProduct } from '../controllers/Product.js';
-
-const productRoutes = express.Router();
-
-productRoutes.use('/get/:id', getProduct);
-productRoutes.use('/all', allProduct);
-
-export default productRoutes;
--- server/utils/token.js
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export const tokenGen = payload => {
-    return jwt.sign({payload}, process.env.SECRET, {algorithm: "HS256"})
-}
